e12bab27db3695f6cfa8af727337d8eddbbc17d3
vika oneil <oneilcode111@gmail.com>
2026-05-24 19:07:19 +1000

    feat: add pagination component due to previous week task

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+type PaginationProps = {
+  onNextPageClick: () => void;
+  onPrevPageClick: () => void;
+  disable: {
+    left: boolean;
+    right: boolean;
+  };
+  nav?: {
+    current: number;
+    total: number;
+  };
+};
+
+const Pagination = (props: PaginationProps) => {
+  const { nav = null, disable, onNextPageClick, onPrevPageClick } = props;
+
+  const handleNextPageClick = () => {
+    onNextPageClick();
+  };
+  const handlePrevPageClick = () => {
+    onPrevPageClick();
+  };
+
+  return (
+    <div className="pagination">
+      <button
+        type="button"
+        onClick={handlePrevPageClick}
+        disabled={disable.left}
+      >
+        {'<'}
+      </button>
+      {nav && (
+        <span>
+          {nav.current} / {nav.total}
+        </span>
+      )}
+      <button
+        type="button"
+        onClick={handleNextPageClick}
+        disabled={disable.right}
+      >
+        {'>'}
+      </button>
+    </div>
+  );
+};
+
+export default React.memo(Pagination);
